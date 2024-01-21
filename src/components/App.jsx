a0c6f63be09a3218df700a44ead8ec15fe09253d
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-21 10:47:56 -0800

    Add current skills

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,70 @@
 // Located in main.jsx
-import { Card, ListGroup, Container, ListGroupItem, CardText, CardTitle} from 'react-bootstrap'
+import { Card, CardTitle, CardImg, Container, CardSubtitle } from 'react-bootstrap'
 import '../styles/App.css'
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
-function App() { 
+import ReactIMG from '../assets/react.png'
+import JavaScriptIMG from '../assets/js.png'
+import CSSIMG from '../assets/css.png'
+import BootstrapIMG from '../assets/bootstrap.png'
+import NodeJsIMG from '../assets/node-js.png'
+import FirebaseIMG from '../assets/firebase.png'
+import CSharpIMG from '../assets/c-sharp.png'
+
+const Item = styled(Paper)(() => ({
+  textAlign: 'center',
+  color:'black',
+  height: 60,
+  lineHeight: '60px',
+}));
+
+export default function App() { 
   return (
     <>
-      { /* Old Project */ }
-      <Container className="current-projects">
-        <Card>
-          <Card.Title className="white"><em>Current Projects!</em></Card.Title>
-          <ListGroup>
-            <ListGroupItem><Card.Link><a href="https://github.com/tdietzel22/resort-website">Resort Website</a></Card.Link></ListGroupItem>
-            <CardText className="cp-describe">This repository practices creating a resort website called Shady Pines with HTML and CSS.</CardText>
-            <ListGroupItem><Card.Link><a href="https://github.com/tdietzel22/wikipedia-rebuild">Wikipedia Rebuild</a></Card.Link></ListGroupItem>
-            <CardText className="cp-describe">This repository is a practice trying to rebuild the homepage of Wikipedia using CSS and HTML.</CardText>
-            <ListGroupItem><Card.Link><a href="https://github.com/tdietzel22/practice-html">Pizza Parlor</a></Card.Link></ListGroupItem>
-            <CardText className="cp-describe">This repository is a practice Pizza Parlor Website using HTML and CSS.</CardText>
-            <ListGroupItem><Card.Link><a href="https://github.com/tdietzel22/cupcake-shop">Cupcake Shop</a></Card.Link></ListGroupItem>
-            <CardText className="cp-describe">This repository is a practice Cupcake Shop using HTML and CSS</CardText>
-            <ListGroupItem><Card.Link><a href="https://github.com/tdietzel22/band-website">Cold Play Website</a></Card.Link></ListGroupItem>
-            <CardText className="cp-describe">This website practice using HTML and CSS to recreate tour dates for Cold Play.</CardText>
-          </ListGroup>
+      <Container style={{ marginTop: '10rem', paddingBottom: '10rem' }}>
+        <Card style={{ paddingBottom: '5rem' }}>
+          <CardTitle style={{fontSize: '3rem', marginBottom: '2rem', color: 'black', fontFamily: 'Comic Sans MS'}}>Skills</CardTitle>
+          <Grid container spacing={2}>
+            <Item elevation={12} style={{width: '90%', marginLeft: 'auto', marginRight: 'auto', height: '15rem', color: 'white', marginBottom: '1rem'}}>
+              <CardSubtitle style={{fontStyle: 'bold', fontSize: '2rem'}}>Front End</CardSubtitle>
+              <div style={{float: 'left', marginLeft: '30rem'}}>
+                <CardImg src={ReactIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>React JS</Card.Text>
+              </div>
+              <div style={{float: 'left', marginLeft: '10rem'}}>
+                <CardImg src={JavaScriptIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>JavaScript</Card.Text>
+              </div>
+              <div style={{float: 'left', marginLeft: '10rem'}}>
+                <CardImg src={CSSIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>CSS</Card.Text>
+              </div>
+              <div style={{float: 'left', marginLeft: '10rem'}}>
+                <CardImg src={BootstrapIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>Bootstrap</Card.Text>
+              </div>
+              </Item>
+            <Item elevation={12} style={{width: '90%', marginLeft: 'auto', marginRight: 'auto', height: '15rem', color: 'white'}}>
+              <CardSubtitle style={{fontStyle: 'bold', fontSize: '2rem'}}>Back End</CardSubtitle>
+              <div style={{float: 'left', marginLeft: '38rem'}}>
+                <CardImg src={CSharpIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>C#</Card.Text>
+              </div>
+              <div style={{float: 'left', marginLeft: '10rem'}}>
+                <CardImg src={FirebaseIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>Firebase</Card.Text>
+              </div>
+              <div style={{float: 'left', marginLeft: '10rem'}}>
+                <CardImg src={NodeJsIMG} width='70rem' height='70rem' style={{marginRight: 'auto', marginLeft: 'auto'}}/>
+                <Card.Text style={{margin: '0rem'}}>NodeJS</Card.Text>
+              </div>
+            </Item>
+          </Grid>
         </Card>
-      </Container>
-      <Container className="about-me">
-      <Card>
-        <CardTitle className="blue"><em>About me!</em></CardTitle>
-        <Card.Subtitle className="red">Interests / Pass Times</Card.Subtitle>
-        <ListGroup>
-          <ListGroupItem>Music</ListGroupItem>
-          <ListGroupItem>Cooking</ListGroupItem>
-          <ListGroupItem>Exploring</ListGroupItem>
-        </ListGroup>
-        <br />
-        <Card.Subtitle className="red">Hobbies</Card.Subtitle>
-        <ListGroup>
-          <ListGroupItem>Working Out</ListGroupItem>
-          <Card.Text className="sub-text">Working out is a great way to keep my mind and body focused.</Card.Text>
-          <ListGroupItem>Fishing</ListGroupItem>
-          <Card.Text className="sub-text">I love fishing for the adventure and occasionally even to keep and eat!</Card.Text>
-          <ListGroupItem>Boxing</ListGroupItem>
-          <Card.Text className="sub-text">I've always been into contact sports and have done everything from wrestling & jui-jitsu, to the most recent boxing.</Card.Text>
-        </ListGroup>
-        <hr />
-        <Card.Subtitle className="red">Skills</Card.Subtitle>
-        <ListGroup>
-          <ListGroupItem>Teamwork</ListGroupItem>
-          <ListGroupItem>Work Ethic</ListGroupItem>
-          <ListGroupItem>Organization</ListGroupItem>
-        </ListGroup>
-      </Card>
-      </Container>
-      <Container className="my-background">
-        <Card.Title className="blue"><em>My background!</em></Card.Title>
-        <Card.Subtitle className="red">Previous Education</Card.Subtitle>
-        <ListGroup>
-          <ListGroupItem>High-School Diploma</ListGroupItem>
-          <ListGroupItem>2 Years of EASA in Highschool</ListGroupItem>
-          <Card.Text className="sub-text">( Engineering & Aerospace Science Academy )</Card.Text>
-        </ListGroup>
-        <Card.Subtitle className="red"> Job Experience</Card.Subtitle>
-        <ListGroup>
-          <ListGroupItem>Farm Equipment Operator</ListGroupItem>
-          <Card.Text className="sub-text">June 2023 - Sep 2023 · 4 mos</Card.Text>
-          <ListGroupItem>Package Handler</ListGroupItem>
-          <Card.Text className="sub-text">Nov 2022 - Jan 2023 · 3 mos</Card.Text>
-          <ListGroupItem>Farm Equipment Operator</ListGroupItem>
-          <Card.Text className="sub-text">Nov 2021 - Sep 2022 · 12 mos</Card.Text>
-        </ListGroup>
-        <hr />
-        <Card.Subtitle className="red">Why Im Enrolled In Epicodus</Card.Subtitle>
-        <Card.Text id="enroll-pg">I first heard about <em>Epicodus</em> in the summer of 2022 as a way for me to expand my learning and dive deeper into coding. During that time I was working peak season on a farm, <strong>90/100</strong> hour weeks, so there wasn't much room for school. Now that I have lots of free time going into winter I decided it would be the perfect time for a career change.</Card.Text>
       </Container>
     </>
   )
 }
-
-export default App;
