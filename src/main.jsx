36de71569be50fbb6526176b7a9e42348ba2d977
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-21 10:49:29 -0800

    Added dark theme

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,27 @@ import TopProjects from './components/TopProjects.jsx'
 import Resume from './components/Resume.jsx'
 import App from './components/App.jsx'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<TopProjects />} />
+        <Route path='/' element={<App />} />
+        <Route path='/TopProjects' element={<TopProjects />} />
         <Route path='/Resume' element={<Resume />} />
-        <Route path='/App' element={<App />} />
       </Routes>
     </Router>
+    </ThemeProvider>
   </React.StrictMode>,
 )
